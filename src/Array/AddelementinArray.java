8620653f9ec3a9b6895c661636896fac412e4784
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-11-12 11:28:55 +0530

    vfn;kdf

--- src/Array/AddelementinArray.java
+++ src/Array/AddelementinArray.java
@@ -7,7 +7,7 @@ public class AddelementinArray {
         int n=sc.nextInt();
         int a=20;
         int[] arr=new int[n+1];
-        System.out.println();
+        System.out.println("enter ");
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
 
