eee0c4cb7e2953aece0f806d9f36cae1d4758f89
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-11-16 14:30:24 +0530

    Bubblesort

--- src/Shorting/Bubblesort.java
+++ src/Shorting/Bubblesort.java
@@ -25,6 +25,7 @@ public class Bubblesort {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println();
         int n=sc.nextInt();
         int[] arr=new int[n];
         for(int i=0; i<n; i++){
