2c35ba55515f16c202576cf034853af1bc96315d
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-11-16 14:31:45 +0530

    Bubblesort

--- src/Shorting/Bubblesort.java
+++ src/Shorting/Bubblesort.java
@@ -22,10 +22,11 @@ public class Bubblesort {
         }
 
     }
+    //time complexity =bido(nsquare)
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println();
+        System.out.println("Enter the numbre");
         int n=sc.nextInt();
         int[] arr=new int[n];
         for(int i=0; i<n; i++){
