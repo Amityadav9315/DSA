4ad20ea7b08ce84c38f934ea50d3058d1b29fbf9
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-11-18 15:44:40 +0530

    ArrayOperation

---
+++ src/Shorting/LinearSearch.java
@@ -0,0 +1,21 @@
+package Shorting;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    static  void linearsearch(){
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] array=new int[n];
+        for(int i=0; i<n; i++){
+            array
+        }
+
+
+    }
+}
