f75f1d75e6e68d4307b29e9fa76ce9db1d3922e8
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-11-19 08:50:44 +0530

    rightrotate

---
+++ src/ArrayPractice/RightRotatearray.java
@@ -0,0 +1,36 @@
+package ArrayPractice;
+
+import java.util.Scanner;
+
+public class RightRotatearray {
+    static  int rightRotate(int n,int arr[]) {
+
+        int temp = arr[n - 1];
+        for (int i = 0; i < n; i++) {
+            arr[n - 1 - i] = arr[n - 2 - i];
+
+
+
+        }
+        arr[0]=temp;
+        for(int x: arr){
+            System.out.println(x);
+        }
+        return -1;
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        rightRotate(n,arr);
+
+
+
+    }
+}
