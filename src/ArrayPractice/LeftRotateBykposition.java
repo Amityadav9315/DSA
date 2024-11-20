f44af5325bcb197f877a9c59c2fecbc5a0ac0a20
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-11-20 15:32:49 +0530

    rfkj;

---
+++ src/ArrayPractice/LeftRotateBykposition.java
@@ -0,0 +1,26 @@
+package ArrayPractice;
+
+import java.util.Scanner;
+
+public class LeftRotateBykposition {
+    static void leftRotatekposition(int n,int arr[],int array2[]){
+         for(int i=0; i<n; i++){
+             
+
+
+         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int[] array=new int[n];
+        int[] array2=new int[k-1];
+        for(int i=0; i<n; i++){
+            array[i]=sc.nextInt();
+
+        }
+
+    }
+}
