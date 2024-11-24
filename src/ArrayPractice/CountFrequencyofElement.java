4751c873f3635e62076951cf224a3ca3cea9e7d3
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-11-24 09:52:55 +0530

    countfrequecy

--- src/ArrayPractice/CountFrequencyofElement.java
+++ src/ArrayPractice/CountFrequencyofElement.java
@@ -1,10 +1,45 @@
 package ArrayPractice;
 
+import java.util.Scanner;
+
 public class CountFrequencyofElement {
+    static  void countFrequency(int n,int array[]){
+        int vistited=-1;
+        int[] freq=new int[array.length];
+
+        for(int i=0; i<n; i++){
+            int count=1;
+            for(int j=i+1; j<n; j++){
+                if(array[i]==array[j]){
+                    count++;
+                    freq[j]=vistited;
+
+                }
+                if(freq[i]!=vistited){
+                    freq[i]=count;
+                }
+
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(freq[i]!=vistited){
+                System.out.println("Element || Frequency");
+                System.out.println(array[i]+"||"+ freq[i]);
+            }
+        }
+    }
 
 
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] array=new int[n];
+        for(int i=0; i<n; i++){
+            array[i]=sc.nextInt();
+        }
+        countFrequency(n,array);
+
 
 
 
