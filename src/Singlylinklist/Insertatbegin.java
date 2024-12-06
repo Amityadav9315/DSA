636c99cf1032daff61545be0182a0eac62241f3c
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-06 19:13:15 +0530

    fwnrg

--- src/Singlylinklist/Insertatbegin.java
+++ src/Singlylinklist/Insertatbegin.java
@@ -1,11 +1,11 @@
 package Singlylinklist;
 
 public class Insertatbegin {
-    //insertion at the begning
-
 
     Node head=null;
-    public void insertatbegin(int item){
+
+    //insertion at begning
+    public  void insertAtBegninning(int item){
         Node newNode=new Node(item);
         if(head==null){
             head=newNode;
@@ -16,6 +16,51 @@ public class Insertatbegin {
     }
     //insert at last
 
+    public void insertAtEnd(int item){
+        Node newNode=new Node(item);
+        if(head==null){
+            head=newNode;
+            return;
+
+        }
+        //important
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+
+        }
+        temp.next=newNode;
+    }
+    //insert at specific position
+    public void insertAtSpecific(int item,int position){
+        Node newNode=new Node(item)
+        if(position<0){
+            System.out.println("Invalid position");
+            return;
+        }
+        if(position==0){
+            newNode.next=head;
+            head=newNode;
+        }
+        Node temp=null;
+        int index=0;
+        while(temp.next!=null){
+            temp=temp.next;
+             index++;
+             if(index==position-1){
+                 break;
+
+             }
+             if(temp==null){
+
+             }
+             //insert
+            newNode.next=temp;
+             temp.next=newNode;
+        }
 
 
+
+
+    }
 }
