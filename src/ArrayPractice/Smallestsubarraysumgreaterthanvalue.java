b6192ef249f1c4f6097ec89287dc1701f7da9c9d
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-10 10:42:29 +0530

    rflgrwr

--- src/ArrayPractice/Smallestsubarraysumgreaterthanvalue.java
+++ src/ArrayPractice/Smallestsubarraysumgreaterthanvalue.java
@@ -8,7 +8,20 @@ public class Smallestsubarraysumgreaterthanvalue {
 
         int value=30;
         int sum=0;
-        for
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++) {
+                sum = array[i] + array[j];
+
+                if(sum>value) {
+                    System.out.println(array[i] + "," + array[j]);
+
+
+                }
+
+
+
+            }
+        }
 
 
     }
@@ -17,10 +30,11 @@ public class Smallestsubarraysumgreaterthanvalue {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int[] array=new int[n];
-        for(int i=0; i<n; i++){
-            array[i]=sc.nextInt();
+        for(int i=0; i<n; i++) {
+            array[i] = sc.nextInt();
 
         }
+        smallestSubarray(n,array);
     }
 
 }
