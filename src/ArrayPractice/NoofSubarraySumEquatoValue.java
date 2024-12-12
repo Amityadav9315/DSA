45d1ee02b533f4577b9acccae7bdc05c595c2add
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-12 11:24:19 +0530

    sgkjgsfkl

---
+++ src/ArrayPractice/NoofSubarraySumEquatoValue.java
@@ -0,0 +1,35 @@
+package ArrayPractice;
+
+import java.util.Scanner;
+
+public class NoofSubarraySumEquatoValue {
+
+    static  void noOfSubarray(int n,int array[]){
+        int value=20;
+        int sum=0;
+        int count=0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                sum=sum+array[i]+array[j];
+                if(sum==value){
+                    count++;
+                }
+
+
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] array=new int[n];
+        for(int i=0; i<n; i++){
+            array[i]=sc.nextInt();
+        }
+        noOfSubarray(n,array);
+
+
+    }
+}
