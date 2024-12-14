ee55c6d144db03a13239a2e2c1310c59b56033ac
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-14 15:33:45 +0530

    countfrequency

--- src/ArrayPractice/countFrequencyofDigit.java
+++ src/ArrayPractice/countFrequencyofDigit.java
@@ -6,6 +6,13 @@ public class countFrequencyofDigit {
 
     static void countFrequency(int n,int array[]){
         int[] array2=new int[n];
+        int visited=-1;
+        for(int i=0; i<n; i++ ){
+            int count=1;
+            for(int j=i+1; j<n; j++){
+                if(array[i])
+            }
+        }
 
 
     }
