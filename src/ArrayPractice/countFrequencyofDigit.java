afb7cba1d7432f1bfd5e8c349f06796a20ec1b6d
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-14 15:31:32 +0530

    countfrequency

---
+++ src/ArrayPractice/countFrequencyofDigit.java
@@ -0,0 +1,21 @@
+package ArrayPractice;
+
+import java.util.Scanner;
+
+public class countFrequencyofDigit {
+
+    static void countFrequency(int n,int array[]){
+        int[] array2=new int[n];
+
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] array=new int[n];
+        for(int i=0; i<n; i++){
+            array[i]=sc.nextInt();
+
+        }
+    }
+}
