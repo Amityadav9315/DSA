71b699230d538d238abf88cb2425670596b4671d
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-01-12 21:16:19 +0530

    febjfe

---
+++ src/Shorting/BubbleSorting.java
@@ -0,0 +1,25 @@
+package Shorting;
+
+import java.awt.datatransfer.StringSelection;
+import java.util.Scanner;
+
+public class BubbleSorting {
+
+    static void bubblesort(int[] arr){
+        for(int i=0; i<arr.length){
+            for(int )
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+
+            arr[i]=sc.nextInt();
+        }
+    }
+}
