ae4e95a60bd3c06ccbf4d6e515c1deb830668c33
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-01-12 20:33:38 +0530

    fekl

---
+++ src/Shorting/MergeSorting.java
@@ -0,0 +1,33 @@
+package Shorting;
+
+import java.util.Scanner;
+
+public class MergeSorting {
+
+
+
+
+    static void Divide(int arr,int st,int ed){
+
+        if(st>=ed){
+            return;
+        }
+
+
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+
+
+    }
+}
