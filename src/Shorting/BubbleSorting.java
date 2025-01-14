0976e91d9db86f8b0f011c6331c4990d06558e0b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-01-14 15:15:05 +0530

    dfgkjds

--- src/Shorting/BubbleSorting.java
+++ src/Shorting/BubbleSorting.java
@@ -6,8 +6,14 @@ import java.util.Scanner;
 public class BubbleSorting {
 
     static void bubblesort(int[] arr){
-        for(int i=0; i<arr.length){
-            for(int
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length-1; j++){
+                    if(arr[j]<arr[j+1]){
+                        int temp=arr[j];
+                        arr[j]=arr[j+1];
+                        arr[j+1]=temp;
+                    }
+            }
 
 
         }
