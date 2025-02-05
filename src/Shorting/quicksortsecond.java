35494fd38dcd214f8e71229c3a4bfb02bc3e78cf
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-05 14:24:07 +0530

    ggkrsj

--- src/Shorting/quicksortsecond.java
+++ src/Shorting/quicksortsecond.java
@@ -1,6 +1,28 @@
 package Shorting;
 
+import java.util.Scanner;
+
 public class quicksortsecond {
+
+
+    static int partition(int[] arr,int low,int high){
+
+
+
+    }
+
+
+    static void quickSort(int[] arr,int low ,int high){
+        if(low<high){
+            int pidx=partition(arr,low,high);
+            quickSort(arr,low,pidx-1);
+            quickSort(arr,pidx+1,high);
+
+        }
+
+
+
+    }
 
 
 
@@ -8,7 +30,13 @@ public class quicksortsecond {
 
 
     public static void main(String[] args) {
-        Sca
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+
 
     }
 }
