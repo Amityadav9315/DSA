8103fcc037e9d47ff369f1d8f004f70bea409ac3
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-05 13:43:18 +0530

    gsknlgrsnk

--- src/Shorting/BubbleSorting.java
+++ src/Shorting/BubbleSorting.java
@@ -11,7 +11,10 @@ public class BubbleSorting {
                     if(arr[j]>arr[j+1]){
                         int temp=arr[j];
                         arr[j]=arr[j+1];
-                        arr[j+1]=temp;                    }
+                        arr[j+1]=temp;
+                    }
+                    fihlfgf
+                            gfdiugfk
             }
 
 
