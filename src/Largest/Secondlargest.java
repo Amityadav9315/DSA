9652d284959f08c62e1aa0f5885b4dbd86615a13
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-07 17:21:06 +0530

    fewihw

--- src/Largest/Secondlargest.java
+++ src/Largest/Secondlargest.java
@@ -1,9 +1,21 @@
 package Largest;
 
+import java.util.Scanner;
+
 public class Secondlargest {
+    static void Secondlargest(){
+
+    }
 
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+
+        }
 
     }
 }
