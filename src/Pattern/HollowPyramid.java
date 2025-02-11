dcb4f3f1d1e901e6498851cc21b02074ecb66f2a
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-11 21:52:00 +0530

    fskn.bf

--- src/Pattern/HollowPyramid.java
+++ src/Pattern/HollowPyramid.java
@@ -24,8 +24,10 @@ public class HollowPyramid {
 
     public static void main(String[] args) {
          Scanner sc=new Scanner(System.in);
-         int n=sc.nextInt();
+       //  int n=sc.nextInt();
         hollowPyraid();
+        
+        
 
 
     }
