650ef30593674cc9bbd6d61624ab08a8c329af99
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-20 00:25:46 +0530

    feklns

---
+++ src/ArrayPractice/PrimeNumber.java
@@ -0,0 +1,22 @@
+package ArrayPractice;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+
+
+    static void primeNumber(int n){
+        int count=1;
+        for(int i=2; i<n; i++ ){
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+    }
+}
