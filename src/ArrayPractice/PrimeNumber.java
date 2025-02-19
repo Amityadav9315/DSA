aad6e7dd626bcaf497d14c1bea69fb39906ac79b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-19 23:54:34 +0530

    fewkrw

--- src/ArrayPractice/PrimeNumber.java
+++ src/ArrayPractice/PrimeNumber.java
@@ -11,7 +11,7 @@ public class PrimeNumber {
         if(n<=1){
             System.out.println("Not  prime number");
         }
-        for(int i=2; i<n; i++ ){
+        for(int i=2; i<=n; i++ ){
           if(n%i==0){
               count++;
           }
