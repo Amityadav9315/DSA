c43c34af7cb018ee855243549d82eaf0d6c21409
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-20 00:11:07 +0530

    fadhj

--- src/TcscodeVita/CosecutivePrimeSum.java
+++ src/TcscodeVita/CosecutivePrimeSum.java
@@ -9,8 +9,15 @@ public class CosecutivePrimeSum {
 
         int count1=1;
         int count2=0;
+        int sum=0;
         for(int  i=2; i<n; i++){
             for(int j=2; j<n; j++){
+                if(j%i==0){
+                    count1++;
+                }
+                if(count1==2){
+                    sum=sum+j;
+                }
 
             }
         }
