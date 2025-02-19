2757559ddced05f6a69cc2f5f4c778d50e83e1dd
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-20 00:30:05 +0530

    feklns

--- src/ArrayPractice/PrimeNumber.java
+++ src/ArrayPractice/PrimeNumber.java
@@ -8,8 +8,19 @@ public class PrimeNumber {
 
     static void primeNumber(int n){
         int count=1;
+        if(n<=1){
+            System.out.println("Not  prime number");
+        }
         for(int i=2; i<n; i++ ){
-
+          if(n%i==0){
+              count++;
+          }
+        }
+        if(count==2){
+            System.out.println("Prime Number");
+        }
+        else{
+            System.out.println("Not Prime Number");
         }
 
     }
@@ -17,6 +28,7 @@ public class PrimeNumber {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        primeNumber(n);
 
     }
 }
